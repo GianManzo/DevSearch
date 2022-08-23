03a83feca33bc83acba3988497a0b6e2f9be7742
Gian Manzo <ggianmanzo@gmail.com>
2022-08-23 15:21:14 -0300

    add header with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from './components/Header/Header';
+import * as C from './Styles-global';
 
 export const App = () => {
   return (
     <>
-      <Header />
+      <C.Container>
+        <Header />
+      </C.Container>
     </>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
-import { Gray, Blue } from '~/variables/colors';
+import { Gray } from '../../variables/colors';
 
 export const Header = styled.header`
-  background: ${Blue.primary};
-  padding: 1rem;
-  color: #fff;
+  font-family: 'Fira Sans', sans-serif;
+  font-weight: bold;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 6.25rem;
+
+  h1 {
+    color: ${Gray[100]};
+  }
 `;
