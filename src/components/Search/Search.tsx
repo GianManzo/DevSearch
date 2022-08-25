31d33e5b72b2e69c13b8e6cf0e5f6ee69dc08080
Gian Manzo <ggianmanzo@gmail.com>
2022-08-25 11:25:59 -0300

    finished custom layout

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,12 @@
 import * as C from './styles';
 import { MagnifyingGlass } from 'phosphor-react';
-import { Blue } from '~/variables/colors';
+import { Green } from '~/variables/colors';
 
 export const Search = () => {
   return (
     <C.Form>
       <C.Wrapper>
-        <MagnifyingGlass size={40} color={Blue.primary} />
+        <MagnifyingGlass size={40} color={Green.light} />
         <input type='text' placeholder='Search GitHub username...' />
         <button type='submit'>Search</button>
       </C.Wrapper>
